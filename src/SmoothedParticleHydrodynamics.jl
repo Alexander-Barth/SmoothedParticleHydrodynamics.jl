d645c4d4a55aaef31b214ae2e9c1f2a4a0a742a0
Alexander Barth <barth.alexander@gmail.com>
2024-05-02 14:36:00 +0200

    rm SpecialFunctions

--- src/SmoothedParticleHydrodynamics.jl
+++ src/SmoothedParticleHydrodynamics.jl
@@ -1,16 +1,11 @@
 module SmoothedParticleHydrodynamics
 
-
 using StaticArrays
 using LinearAlgebra
 using Random
-using SpecialFunctions
-
 
 include("types.jl")
 include("kernels.jl")
 include("dynamics.jl")
-
-
 
 end # module SmoothedParticleHydrodynamics
